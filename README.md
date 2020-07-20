c7b6993d4e2132af6650756c901ad2b4faaee435
zhangyi <zhangyi@yeeyoon.com>
2020-07-20 16:34:12 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+<h2 align= center> </h2>
+
+<h5 align=right> 极客点儿 </h5>
+<p align=right> 2020-07-20 </p>
+
---
+++ common/phpmyadmin/shortcuts_handler.js
@@ -0,0 +1,14 @@
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * @fileoverview    Handle shortcuts in various pages
+ * @name            Shortcuts handler
+ *
+ * @requires    jQuery
+ * @requires    jQueryUI
+ */
+
+/**
+ * Register key events on load
+ */
+$(document).ready(function () {
+});
